8b7a706aa41e42331ec56f9a045d7d5fce0e5c56
FMGS666 <b00794799@essec.edu>
2024-10-15 11:42:08 +0200

    fixed problem with cfgen encoder and autoencoder and removed the CFGenAE class that was no longer used nor useful

--- src/cfp/networks/_cfgen_ae.py
+++ src/cfp/networks/_cfgen_ae.py
@@ -19,7 +19,6 @@ from cfp.networks._set_encoders import MLPBlock, BaseModule
 __all__ = [
     "CFGenEncoder",
     "CFGenDecoder",
-    "CFGenAE"
 ]
 
 
@@ -45,8 +44,6 @@ class CFGenEncoder(BaseModule):
     input_dim: dict[str, int]
     encoder_kwargs: dict[str, dict[str, Any]]
     covariate_specific_theta: bool
-    conditioning_covariate: str
-    n_cat: int
     is_binarized: bool
     encoder_multimodal_joint_layers: dict[str, Any] | None
 
@@ -54,14 +51,13 @@ class CFGenEncoder(BaseModule):
     def setup(self) -> None:
         """Initialize the module."""
         
-        # copying the encoder kwargs attribures to modify it safely
         encoder_kwargs = self.encoder_kwargs.unfreeze()
         # List of modalities present in the data 
-        self.modality_list = list(encoder_kwargs.keys())
+        modality_list = list(encoder_kwargs.keys())
 
         # initializing the modality specific components
         encoder = dict()
-        for mod in self.modality_list:
+        for mod in modality_list:
             encoder_kwargs[mod]["dims"] = [self.input_dim[mod], *self.encoder_kwargs[mod]["dims"]]
             encoder[mod] = MLPBlock(**encoder_kwargs[mod])
         self.encoder = encoder
@@ -77,8 +73,10 @@ class CFGenEncoder(BaseModule):
             training: bool
         ) -> dict[str, jnp.ndarray] | jnp.ndarray:
         """Encodes the Input"""
+        modality_list = list(self.encoder_kwargs.keys())
         z = {}
-        for mod in self.modality_list:
+        #for mod in self.modality_list:
+        for mod in modality_list:
             z_mod = self.encoder[mod](X[mod], training = training)
             z[mod] = z_mod
         
@@ -107,11 +105,9 @@ class CFGenDecoder(BaseModule):
     is_binarized: bool
         Whether the input data is binarized 
     """
-    #input_dim: dict[str, int]
+    input_dim: dict[str, int]
     encoder_kwargs: dict[str, dict[str, Any]]
     covariate_specific_theta: bool
-    conditioning_covariate: str
-    n_cat: int
     is_binarized: bool
     encoder_multimodal_joint_layers: dict[str, Any] | None
 
@@ -122,10 +118,10 @@ class CFGenDecoder(BaseModule):
         # copying the encoder kwargs attribures to modify it safely
         encoder_kwargs = self.encoder_kwargs.unfreeze()
         # List of modalities present in the data 
-        self.modality_list = list(encoder_kwargs.keys())
+        modality_list = list(encoder_kwargs.keys())
         # initializing the modality specific components
         decoder = {}
-        for mod in self.modality_list:
+        for mod in modality_list:
             encoder_kwargs[mod]["dims"] = [self.input_dim[mod], *self.encoder_kwargs[mod]["dims"]]
             if self.encoder_multimodal_joint_layers:
                 encoder_kwargs[mod]["dims"].append(self.encoder_multimodal_joint_layers["dims"][-1])
@@ -140,8 +136,9 @@ class CFGenDecoder(BaseModule):
             training: bool
         ) -> dict[str, jnp.ndarray]:
         """Encodes the Input"""
+        modality_list = list(self.encoder_kwargs.keys())
         mu_hat = {}
-        for mod in self.modality_list:
+        for mod in modality_list:
             if not self.encoder_multimodal_joint_layers:
                 x_mod = self.decoder[mod](z[mod], training)
             else:
@@ -154,67 +151,3 @@ class CFGenDecoder(BaseModule):
                 mu_hat_mod = nn.sigmoid(x_mod)
             mu_hat[mod] = mu_hat_mod
         return mu_hat
-
-class CFGenAE:
-    """
-    Implements the AutoEncoder architecture of the CFGen model
-
-    Parameters
-    ----------
-    input_dim: dict[str, int]
-        The dimensionality of the input for each modality
-    encoder_kwargs: dict[str, dict[str, Any]]
-        The key-word arguments for initializing the encoder (one for each modality)
-    covariate_specific_theta: bool
-        Whether to use a dispersion coefficient for each covariate
-    is_binarized: bool
-        Whether the input data is binarized 
-    encoder_multimodal_joint_layers: dict[str, Any] | None
-        The layers for the optional joint encoer across multiple modalities
-    """
-    input_dim: dict[str, int]
-    encoder_kwargs: dict[str, dict[str, Any]]
-    covariate_specific_theta: bool
-    is_binarized: bool
-    encoder_multimodal_joint_layers: dict[str, Any] | None
-
-    def setup(
-            self,
-        ) -> None:
-        """"""
-        self.modality_list = list(self.encoder_kwargs.keys())
-        # Learnable Theta Parameter
-        in_dim_rna = self.input_dim["rna"]
-        if not self.covariate_specific_theta:
-            shape = (in_dim_rna)
-        else:
-            shape = (n_cat, in_dim_rna)
-        self.param('theta', multivariate_normal, shape = shape, dim = in_dim_rna, mean = 0.0, cov = 1.0)
-        
-        self.encoder = CFGenEncoder( 
-            input_dim = self.input_dim,
-            encoder_kwargs = self.encoder_kwargs,
-            covariate_specific_theta = self.covariate_specific_theta,
-            is_binarized = self.is_binarized,
-            encoder_multimodal_joint_layers = self.encoder_multimodal_joint_layers
-        )
-        self.decoder = CFGenDecoder(
-            input_dim = self.input_dim,
-            encoder_kwargs = self.encoder_kwargs,
-            covariate_specific_theta = self.covariate_specific_theta,
-            is_binarized = self.is_binarized,
-            encoder_multimodal_joint_layers = self.encoder_multimodal_joint_layers
-        )
-    
-    def __call__(
-            self,
-            x: dict[str, jnp.ndarray],
-            training: bool   
-        ) -> dict[str, jnp.ndarray]:
-        ## retrieving size factors
-        size_factor = {}
-        for mod in self.modality_list:
-            size_factor[mod] = jnp.sum(x[mod], axis = 1, keepdims = True)
-        z = self.encoder(x, training)
-        x_hat = self.decoder(z, size_factor, training)
-        return x_hat
